54aaedbe78abb29125b43a3a905cec802caa81b0
sranav <sranavb@gmail.com>
2025-09-11 18:30:06 +0530

    HomeButtonCompleted

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,109 +9,56 @@ import { Image, ImageBackground, Text } from "react-native";
 
 
 export default function TabsLayout() {
-
-    return (
-
-        <Tabs
-
-            screenOptions={{
-
-            tabBarActiveTintColor: "hotpink",
-
-            tabBarInactiveTintColor: "gray",
-
-            tabBarStyle: {
-
-                backgroundColor: "green",
-
-            },
-
-            }}
-
-        >
-
-            <Tabs.Screen
-
-            name="index"
-
-            options={{
-
-                title: "Veedu",
-
-                headerShown: false,
-
-                tabBarIcon: ({ size }) => (
-
-                <ImageBackground
-
-                    source={images.highlight}
-
-                    style={{
-
-                    width: size * 2, // increase length
-
-                    height: size,
-
-                    flexDirection: "row",
-
-                    alignItems: "center",
-
-                    justifyContent: "center",
-
-// curved edges
-overflow: "hidden",
-}}
-
-resizeMode="cover"
->
-    <Image
-        source={icons.home}
-        style={{
-            width: size * 0.6,
-            height: size * 0.6,
-            marginRight: 8,
-        }}
-    />
-    <Text style={{ fontSize: 14, color: "white", fontWeight: "bold" }}>
-        Home
-    </Text>
-
-                </ImageBackground>
-
-                ),
-
-            }}
-
-            />
-
-            <Tabs.Screen
-
-            name="profile"
-
-            options={{ title: "Profile", headerShown: false }}
-
-            />
-
-            <Tabs.Screen
-
-            name="saved"
-
-            options={{ title: "Saved", headerShown: false }}
-
-            />
-
-            <Tabs.Screen
-
-            name="search"
-
-            options={{ title: "Search", headerShown: false }}
-
-            />
-
-        </Tabs>
-
-    );
-
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: "hotpink",
+                tabBarInactiveTintColor: "gray",
+                tabBarStyle: {
+                    backgroundColor: "green",
+                },
+            }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "",
+                    headerShown: false,
+                    tabBarIcon: ({ size }) => (
+                        <ImageBackground
+                            source={images.highlight}
+                            className="flex flex-row w-1/2 flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+                                >
+                                <Image
+                                    source={icons.home}
+                                    style={{
+                                        width: size, // make icon bigger
+                                        height: size,
+                                        marginRight: 8,
+                                    }}
+                                    resizeMode="contain"
+                                />
+                                <Text style={{ fontSize: 16, color: "white", fontWeight: "bold" }}>
+                                    Home
+                                </Text>
+                        </ImageBackground>
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="profile"
+                options={{ title: "Profile", headerShown: false }}
+            />
+            <Tabs.Screen
+                name="saved"
+                options={{ title: "Saved", headerShown: false }}
+            />
+            <Tabs.Screen
+                name="search"
+                options={{ title: "Search", headerShown: false }}
+            />
+        </Tabs>
+    );
 }
 
 
