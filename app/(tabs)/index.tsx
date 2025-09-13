74e52a4fc72fd1ed0c2d5f0e33daa92d3cd77cec
sranav <sranavb@gmail.com>
2025-09-13 11:58:28 +0530

    Index Almost Done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,26 @@
-import { Text, View } from "react-native";
-import {Link} from "expo-router";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="bg-purple-500 flex-1 justify-center items-center">
-      <Text className="text-dark-200 font-bold text-3xl">Welcome!.</Text>
+    <View className="flex-1 bg-primary">
+      <Image 
+        source={images.bg} 
+        className="absolute w-1/2 h-full z-0" 
+      />
+
+      <ScrollView className="flex-1 px-5">
+        <Image 
+          source={icons.logo} 
+          style={{ width: 50, height: 50 }}
+          className="mt-20 mb-5 mx-auto" 
+        />
+      </ScrollView>
     </View>
   );
 }
+
+
+
+
--- constants/images.ts
+++ constants/images.ts
@@ -1,4 +1,4 @@
-import bg from '../assets/images/bg.png';
+import bg from '../assets/images/bg.jpg';
 import highlight from '../assets/images/highlight.webp';
 import logo from '../assets/images/logo.png';
 import rankingGradient from '../assets/images/rankingGradient.jpg';
