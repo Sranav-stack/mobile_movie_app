dab505d391542fb73dcabdc6094096717e7b987f
sranav <sranavb@gmail.com>
2025-09-20 01:47:09 +0530

    flatlist

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,14 +4,15 @@ import { images } from "@/constants/images";
 import { fetchPopularMovies } from "@/services/api";
 import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, View } from "react-native";
-
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
-  const {data : movies,loading:moviesLoading,error:movieError} = useFetch(()=>fetchPopularMovies({query:''}))
+  const { data: movies, loading: moviesLoading, error: moviesError } = useFetch(() =>
+    fetchPopularMovies({ query: "" })
+  );
+
   return (
-    
     <View className="flex-1 bg-primary">
       <Image 
         source={images.bg} 
@@ -23,14 +24,41 @@ export default function Index() {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ flexGrow: 1, paddingBottom: 20 }}
       >
+        {/* Logo */}
         <Image 
           source={icons.logo} 
           style={{ width: 50, height: 50 }}
           className="mt-5 mb-5 mx-auto" 
         />
 
-        <SearchBar onPress={()=>router.push("/search")} placeholder="Search for a movie"/>
+        {/* Conditional rendering */}
+        {moviesLoading ? (
+          <ActivityIndicator 
+            size="large" 
+            color="#0000ff" 
+            className="mt-10 self-center" 
+          />
+        ) : moviesError ? (
+          <Text className="text-red-500 mt-5">Error: {moviesError.message}</Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar onPress={() => router.push("/search")} placeholder="Search for a movie" />
+            <Text className="mt-5 text-white text-lg font-bold">Latest Movies</Text>
+            <FlatList
+              data={movies ?? []}
+              renderItem={({ item }) => (
+               <View className="flex-1 m-2">
+                 <Text className="text-white text-center">{item.title}</Text>
+               </View>
+              )}
+              numColumns={3}
+              keyExtractor={(item) => item.id.toString()}
 
+            />
+          </View>
+        )}
+
+        {/* Temporary placeholder movie list */}
         {Array.from({ length: 20 }).map((_, i) => (
           <View key={i} className="h-20 bg-white my-2 rounded-xl" />
         ))}
@@ -38,8 +66,3 @@ export default function Index() {
     </View>
   );
 }
-
-
-
-
-
