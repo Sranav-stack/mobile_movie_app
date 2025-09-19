48392c162808c8ead25f569389d6f490e5777ec9
sranav <sranavb@gmail.com>
2025-09-20 00:59:42 +0530

    Cheriya Mattangal

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,15 @@
 import { SearchBar } from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { fetchPopularMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
 import { Image, ScrollView, View } from "react-native";
 
 
 export default function Index() {
   const router = useRouter();
+  const {data : movies,loading:moviesLoading,error:movieError} = useFetch(()=>fetchPopularMovies({query:''}))
   return (
     
     <View className="flex-1 bg-primary">
